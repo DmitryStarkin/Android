7725f591e2804c71a444db8cadc77f81741fe505
StarkinDG <380506803080@yandex.ua>
2017-03-01 18:32:42 +0200

    bug fixed, done

--- task5/src/main/java/com/hplasplas/task5/Activitys/SettingsActivity.java
+++ task5/src/main/java/com/hplasplas/task5/Activitys/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.hplasplas.task5.R;
@@ -16,39 +17,44 @@ import static com.hplasplas.task5.Setting.Constants.DEBUG;
 public class SettingsActivity extends PreferenceActivity {
     
     private final String TAG = getClass().getSimpleName();
-    
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    
+
+        super.onCreate(savedInstanceState);
         if (DEBUG) {
             Log.d(TAG, "onCreate: ");
         }
-        super.onCreate(savedInstanceState);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+
         Intent intent = getIntent();
-        if(intent != null) {
-            if(intent.getBooleanExtra("currentNotificationRead", false)){
+
+        if (intent != null) {
+            if (DEBUG) {
+                Log.d(TAG, "onCreate: currentNotificationRead " + intent.getBooleanExtra("currentNotificationRead", false));
+            }
+            if (intent.getBooleanExtra("currentNotificationRead", false)) {
                 Intent startServiceIntent = new Intent(this.getApplicationContext(), NotificationService.class);
                 startServiceIntent.putExtra("currentNotificationRead", true);
                 startService(startServiceIntent);
             }
         }
 
-
-
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
-    
+
     @Override
     protected void onStop() {
-        
-        super.onStop();
+
+        Intent newIntent = new Intent(this.getApplicationContext(), NotificationService.class);
+        startService(newIntent);
         if (DEBUG) {
             Log.d(TAG, "onStop: ");
         }
-        Intent newIntent = new Intent(this.getApplicationContext(), NotificationService.class);
-        startService(newIntent);
+        super.onStop();
     }
-    
+
     public static class MyPreferenceFragment extends PreferenceFragment {
         
         private final String TAG = getClass().getSimpleName();
