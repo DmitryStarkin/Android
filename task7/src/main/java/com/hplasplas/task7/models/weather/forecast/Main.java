3573313ff5162205cd7cb475ff3686a531b707e9
StarkinDG <380506803080@yandex.ua>
2017-04-30 18:06:02 +0300

    Task7 fix integer temp

--- task7/src/main/java/com/hplasplas/task7/models/weather/forecast/Main.java
+++ task7/src/main/java/com/hplasplas/task7/models/weather/forecast/Main.java
@@ -29,7 +29,7 @@ public class Main {
     private long humidity;
     @SerializedName("temp_kf")
     @Expose
-    private Integer tempKf;
+    private double tempKf;
 
     public double getTemp() {
         return temp;
@@ -87,11 +87,11 @@ public class Main {
         this.humidity = humidity;
     }
 
-    public Integer getTempKf() {
+    public double getTempKf() {
         return tempKf;
     }
 
-    public void setTempKf(Integer tempKf) {
+    public void setTempKf(double tempKf) {
         this.tempKf = tempKf;
     }
 
